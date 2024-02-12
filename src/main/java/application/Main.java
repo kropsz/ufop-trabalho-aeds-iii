2ede1a46ff7e1e4a1bffe22d95aefb20a838ade2
Pedro Esteves <kropinho211@outlook.com>
2024-02-12 19:55:30 -0300

    "Implementação do algoritmo de Dijkstra"

---
+++ src/main/java/application/Main.java
@@ -0,0 +1,10 @@
+package application;
+
+import entities.BitmapReader;
+
+public class Main {
+    public static void main(String[] args) {
+        BitmapReader reader = new BitmapReader();
+        reader.setVisible(true);
+    }
+}
--- src/main/java/entities/GrafoDePixels.java
+++ src/main/java/entities/GrafoDePixels.java
@@ -3,56 +3,115 @@ package entities;
 import java.awt.image.BufferedImage;
 import java.awt.Color;
 import java.awt.Font;
-import java.awt.geom.Point2D;
 import java.util.*;
-import org.jgrapht.graph.DefaultUndirectedGraph;
-import org.jgrapht.graph.DefaultEdge;
+import org.jgrapht.graph.DefaultWeightedEdge;
+import org.jgrapht.graph.SimpleWeightedGraph;
 import javax.swing.JLabel;
 
-
 public class GrafoDePixels {
-    private DefaultUndirectedGraph<Point2D, DefaultEdge> grafo;
-    private BufferedImage image;
+    private SimpleWeightedGraph<Point3D, DefaultWeightedEdge> grafo;
+    private BufferedImage[] imagens;
 
-    public GrafoDePixels(BufferedImage image) {
-        this.grafo = new DefaultUndirectedGraph<>(DefaultEdge.class);
-        this.image = image;
+    public GrafoDePixels(BufferedImage[] imagens) {
+        this.grafo = new SimpleWeightedGraph<>(DefaultWeightedEdge.class);
+        this.imagens = imagens;
     }
 
-    public void adicionarPixel(int x, int y) {
-        Point2D coordenadaPixel = new Point2D.Double(x, y);
+    public void adicionarPixel(int x, int y, int z) {
+        Point3D coordenadaPixel = new Point3D(x, y, z);
         grafo.addVertex(coordenadaPixel);
     }
 
-    public void adicionarConexao(int x1, int y1, int x2, int y2) {
-        Point2D coordenadaPixel1 = new Point2D.Double(x1, y1);
-        Point2D coordenadaPixel2 = new Point2D.Double(x2, y2);
-        grafo.addEdge(coordenadaPixel1, coordenadaPixel2);
+    public void adicionarConexao(Point3D ponto1, Point3D ponto2, double peso) {
+        DefaultWeightedEdge edge = grafo.addEdge(ponto1, ponto2);
+        grafo.setEdgeWeight(edge, peso);
     }
 
     public void adicionarArestasVizinhas() {
-        int largura = image.getWidth();
-        int altura = image.getHeight();
-        for (int y = 0; y < altura; y++) {
-            for (int x = 0; x < largura; x++) {
-                if (x < largura - 1 && !isPixelPreto(x, y) && !isPixelPreto(x + 1, y)) {
-                    adicionarConexao(x, y, x + 1, y);
-                }
-                if (y < altura - 1 && !isPixelPreto(x, y) && !isPixelPreto(x, y + 1)) {
-                    adicionarConexao(x, y, x, y + 1);
+        int largura = imagens[0].getWidth();
+        int altura = imagens[0].getHeight();
+        int profundidade = imagens.length;
+        for (int z = 0; z < profundidade; z++) {
+            for (int y = 0; y < altura; y++) {
+                for (int x = 0; x < largura; x++) {
+                    Point3D pontoAtual = new Point3D(x, y, z);
+                    if (x < largura - 1) {
+                        Point3D pontoVizinho = new Point3D(x + 1, y, z);
+                        if (!isPixelObstaculo(pontoAtual) && !isPixelObstaculo(pontoVizinho)) {
+                            adicionarConexao(pontoAtual, pontoVizinho, calcularPeso(pontoAtual, pontoVizinho));
+                        }
+                    }
+                    if (y < altura - 1) {
+                        Point3D pontoVizinho = new Point3D(x, y + 1, z);
+                        if (!isPixelObstaculo(pontoAtual) && !isPixelObstaculo(pontoVizinho)) {
+                            adicionarConexao(pontoAtual, pontoVizinho, calcularPeso(pontoAtual, pontoVizinho));
+                        }
+                    }
+                    if (z < profundidade - 1) {
+                        Point3D pontoVizinho = new Point3D(x, y, z + 1);
+                        if (!isPixelObstaculo(pontoAtual) && !isPixelObstaculo(pontoVizinho)) {
+                            adicionarConexao(pontoAtual, pontoVizinho, calcularPeso(pontoAtual, pontoVizinho));
+                        }
+                    }
                 }
             }
         }
     }
-    
-    public Point2D buscarNoPorCor(Color cor) {
-        Set<Point2D> nosCorrespondentes = new HashSet<>();
-        Set<Point2D> vertices = grafo.vertexSet();
 
-        for (Point2D ponto : vertices) {
+    private double calcularPeso(Point3D ponto1, Point3D ponto2) {
+        int largura = imagens[0].getWidth();
+        int altura = imagens[0].getHeight();
+        int profundidade = imagens.length;
+        double peso = 1.0;
+
+        for (int k = Math.max(0, (int) ponto1.getZ() - 1); k <= Math.min(profundidade - 1,
+                (int) ponto1.getZ() + 1); k++) {
+            for (int i = Math.max(0, (int) ponto1.getX() - 1); i <= Math.min(largura - 1,
+                    (int) ponto1.getX() + 1); i++) {
+                for (int j = Math.max(0, (int) ponto1.getY() - 1); j <= Math.min(altura - 1,
+                        (int) ponto1.getY() + 1); j++) {
+                    Point3D ponto = new Point3D(i, j, k);
+                    if (isPixelPreto(ponto)) {
+                        peso += 1000.0;
+                    } else if (isPixelCinzaEscuro(ponto)) {
+                        peso += 500.0;
+                    } else if (isPixelCinzaClaro(ponto)) {
+                        peso += 200.0;
+                    }
+                }
+            }
+        }
+
+        for (int k = Math.max(0, (int) ponto2.getZ() - 1); k <= Math.min(profundidade - 1,
+                (int) ponto2.getZ() + 1); k++) {
+            for (int i = Math.max(0, (int) ponto2.getX() - 1); i <= Math.min(largura - 1,
+                    (int) ponto2.getX() + 1); i++) {
+                for (int j = Math.max(0, (int) ponto2.getY() - 1); j <= Math.min(altura - 1,
+                        (int) ponto2.getY() + 1); j++) {
+                    Point3D ponto = new Point3D(i, j, k);
+                    if (isPixelPreto(ponto)) {
+                        peso += 1000.0;
+                    } else if (isPixelCinzaEscuro(ponto)) {
+                        peso += 500.0;
+                    } else if (isPixelCinzaClaro(ponto)) {
+                        peso += 200.0;
+                    }
+                }
+            }
+        }
+
+        return peso;
+    }
+
+    public List<Point3D> buscarNosPorCor(Color cor) {
+        List<Point3D> nosCorrespondentes = new ArrayList<>();
+        Set<Point3D> vertices = grafo.vertexSet();
+
+        for (Point3D ponto : vertices) {
             int x = (int) ponto.getX();
             int y = (int) ponto.getY();
-            int rgb = image.getRGB(x, y);
+            int z = (int) ponto.getZ();
+            int rgb = imagens[z].getRGB(x, y);
             Color pixelColor = new Color(rgb);
 
             if (pixelColor.equals(cor)) {
@@ -60,26 +119,45 @@ public class GrafoDePixels {
             }
         }
 
-        if (!nosCorrespondentes.isEmpty()) {
-            return nosCorrespondentes.iterator().next();
-        } else {
-            return null;
-        }
+        return nosCorrespondentes;
     }
 
-    public List<Point2D> buscaEmLargura(Point2D pontoInicial, Point2D pontoDestino) {
-        List<Point2D> caminho = new ArrayList<>();
-        Queue<Point2D> fila = new LinkedList<>();
-        Map<Point2D, Point2D> predecessores = new HashMap<>();
-        Set<Point2D> visitados = new HashSet<>();
+    public Point3D buscarNoPorCor(Color cor) {
+        Set<Point3D> vertices = grafo.vertexSet();
+
+        for (Point3D ponto : vertices) {
+            int x = (int) ponto.getX();
+            int y = (int) ponto.getY();
+            int z = (int) ponto.getZ();
+            int rgb = imagens[z].getRGB(x, y);
+            Color pixelColor = new Color(rgb);
+
+            if (pixelColor.equals(cor)) {
+                return ponto;
+            }
+        }
+
+        return null;
+    }
+
+    public List<Point3D> dijkstra(Point3D pontoInicial, Point3D pontoDestino) {
+        Map<Point3D, Double> distancias = new HashMap<>();
+        Map<Point3D, Point3D> predecessores = new HashMap<>();
+        PriorityQueue<Point3D> fila = new PriorityQueue<>(Comparator.comparingDouble(distancias::get));
+
+        for (Point3D ponto : todosOsPontos()) {
+            distancias.put(ponto, Double.POSITIVE_INFINITY);
+        }
+        distancias.put(pontoInicial, 0.0);
 
         fila.add(pontoInicial);
-        visitados.add(pontoInicial);
 
         while (!fila.isEmpty()) {
-            Point2D atual = fila.poll();
+            Point3D atual = fila.poll();
+
             if (atual.equals(pontoDestino)) {
-                Point2D ponto = pontoDestino;
+                List<Point3D> caminho = new ArrayList<>();
+                Point3D ponto = pontoDestino;
                 while (ponto != null) {
                     caminho.add(ponto);
                     ponto = predecessores.get(ponto);
@@ -88,79 +166,145 @@ public class GrafoDePixels {
                 return caminho;
             }
 
-            Set<Point2D> vizinhos = obterVizinhos(atual);
-            for (Point2D vizinho : vizinhos) {
-                if (!visitados.contains(vizinho)) {
-                    fila.add(vizinho);
-                    visitados.add(vizinho);
+            Set<Point3D> vizinhos = obterVizinhos(atual);
+            for (Point3D vizinho : vizinhos) {
+                double peso = calcularPeso(atual, vizinho);
+                double distanciaAlternativa = distancias.get(atual) + peso;
+                if (distanciaAlternativa < distancias.get(vizinho)) {
+                    fila.remove(vizinho);
+                    distancias.put(vizinho, distanciaAlternativa);
                     predecessores.put(vizinho, atual);
+                    fila.add(vizinho);
                 }
             }
         }
 
-        return caminho;
+        return null;
     }
 
-    private Set<Point2D> obterVizinhos(Point2D ponto) {
-        Set<Point2D> vizinhos = new HashSet<>();
+    private Set<Point3D> todosOsPontos() {
+        Set<Point3D> pontos = new HashSet<>();
+        int largura = imagens[0].getWidth();
+        int altura = imagens[0].getHeight();
+        int profundidade = imagens.length;
+
+        for (int z = 0; z < profundidade; z++) {
+            for (int x = 0; x < largura; x++) {
+                for (int y = 0; y < altura; y++) {
+                    pontos.add(new Point3D(x, y, z));
+                }
+            }
+        }
+
+        return pontos;
+    }
+
+    private Set<Point3D> obterVizinhos(Point3D ponto) {
+        Set<Point3D> vizinhos = new HashSet<>();
         int x = (int) ponto.getX();
         int y = (int) ponto.getY();
-        int largura = image.getWidth();
-        int altura = image.getHeight();
+        int z = (int) ponto.getZ();
+        int largura = imagens[0].getWidth();
+        int altura = imagens[0].getHeight();
+        int profundidade = imagens.length;
 
         if (x > 0) {
-            Point2D vizinhoEsquerda = new Point2D.Double(x - 1, y);
-            if (!isPixelPreto((int)vizinhoEsquerda.getX(), (int)vizinhoEsquerda.getY())) {
+            Point3D vizinhoEsquerda = new Point3D(x - 1, y, z);
+            if (!isPixelObstaculo(vizinhoEsquerda)) {
                 vizinhos.add(vizinhoEsquerda);
             }
         }
         if (x < largura - 1) {
-            Point2D vizinhoDireita = new Point2D.Double(x + 1, y);
-            if (!isPixelPreto((int)vizinhoDireita.getX(), (int)vizinhoDireita.getY())) {
+            Point3D vizinhoDireita = new Point3D(x + 1, y, z);
+            if (!isPixelObstaculo(vizinhoDireita)) {
                 vizinhos.add(vizinhoDireita);
             }
         }
         if (y > 0) {
-            Point2D vizinhoCima = new Point2D.Double(x, y - 1);
-            if (!isPixelPreto((int)vizinhoCima.getX(), (int)vizinhoCima.getY())) {
+            Point3D vizinhoCima = new Point3D(x, y - 1, z);
+            if (!isPixelObstaculo(vizinhoCima)) {
                 vizinhos.add(vizinhoCima);
             }
         }
         if (y < altura - 1) {
-            Point2D vizinhoBaixo = new Point2D.Double(x, y + 1);
-            if (!isPixelPreto((int)vizinhoBaixo.getX(), (int)vizinhoBaixo.getY())) {
+            Point3D vizinhoBaixo = new Point3D(x, y + 1, z);
+            if (!isPixelObstaculo(vizinhoBaixo)) {
                 vizinhos.add(vizinhoBaixo);
+            }
+        }
+        if (z > 0) {
+            Point3D vizinhoFrente = new Point3D(x, y, z - 1);
+            if (!isPixelObstaculo(vizinhoFrente)) {
+                vizinhos.add(vizinhoFrente);
+            }
+        }
+        if (z < profundidade - 1) {
+            Point3D vizinhoAtras = new Point3D(x, y, z + 1);
+            if (!isPixelObstaculo(vizinhoAtras)) {
+                vizinhos.add(vizinhoAtras);
             }
         }
 
         return vizinhos;
     }
 
-    private boolean isPixelPreto(int x, int y) {
-        int rgb = image.getRGB(x, y);
+    private boolean isPixelObstaculo(Point3D ponto) {
+        int x = (int) ponto.getX();
+        int y = (int) ponto.getY();
+        int z = (int) ponto.getZ();
+        int rgb = imagens[z].getRGB(x, y);
+        Color cor = new Color(rgb);
+        return cor.equals(Color.BLACK) || cor.equals(Color.DARK_GRAY) || cor.equals(Color.GRAY);
+    }
+
+    private boolean isPixelPreto(Point3D ponto) {
+        int x = (int) ponto.getX();
+        int y = (int) ponto.getY();
+        int z = (int) ponto.getZ();
+        int rgb = imagens[z].getRGB(x, y);
         Color cor = new Color(rgb);
         return cor.equals(Color.BLACK);
     }
 
-    public void exibirSequenciaDePassos(List<Point2D> caminho, JLabel label) {
+    private boolean isPixelCinzaEscuro(Point3D ponto) {
+        int x = (int) ponto.getX();
+        int y = (int) ponto.getY();
+        int z = (int) ponto.getZ();
+        int rgb = imagens[z].getRGB(x, y);
+        Color cor = new Color(rgb);
+        return cor.equals(Color.DARK_GRAY);
+    }
+
+    private boolean isPixelCinzaClaro(Point3D ponto) {
+        int x = (int) ponto.getX();
+        int y = (int) ponto.getY();
+        int z = (int) ponto.getZ();
+        int rgb = imagens[z].getRGB(x, y);
+        Color cor = new Color(rgb);
+        return cor.equals(Color.GRAY);
+    }
+
+    public void exibirSequenciaDePassos(List<Point3D> caminho, JLabel label) {
         StringBuilder sequencia = new StringBuilder();
         for (int i = 0; i < caminho.size() - 1; i++) {
-            Point2D atual = caminho.get(i);
-            Point2D proximo = caminho.get(i + 1);
+            Point3D atual = caminho.get(i);
+            Point3D proximo = caminho.get(i + 1);
             if (proximo.getX() > atual.getX()) {
-                sequencia.append("→ "); // Setas para a direita
+                sequencia.append("→ ");
             } else if (proximo.getX() < atual.getX()) {
-                sequencia.append("← "); // Setas para a esquerda
+                sequencia.append("← "); 
             } else if (proximo.getY() > atual.getY()) {
-                sequencia.append("↓ "); // Setas para baixo
+                sequencia.append("↓ "); 
             } else if (proximo.getY() < atual.getY()) {
-                sequencia.append("↑ "); // Setas para cima
+                sequencia.append("↑ "); 
+            } else if (proximo.getZ() > atual.getZ()) {
+                sequencia.append("⇡ "); 
+            } else if (proximo.getZ() < atual.getZ()) {
+                sequencia.append("⇣ "); 
             }
         }
         label.setText(sequencia.toString());
         label.setFont(new Font("Arial", Font.PLAIN, 20));
     }
-    
-    
-    
+
 }
